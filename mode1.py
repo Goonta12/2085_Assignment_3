2e4601f40c63180b04bb8644895221a066e8fd19
Goonta11 <goonta2988@gmail.com>
2023-10-22 15:17:22 +1100

    update

--- mode1.py
+++ mode1.py
@@ -10,7 +10,32 @@ class Mode1Navigator:
 
     def __init__(self, islands: list[Island], crew: int) -> None:
         """
-        Student-TODO: Best/Worst Case
+        Worst Case O( N * comp )
+        Best Case: O( N * comp )
+        where N is the number of islands given in island.
+
+        Data Structure:
+        The data structure used in the function was a binary tree.
+        This was, firstly, because the assignment outlined that the
+        best and worst case of using the class function would be 
+        O( LogN ) and O( N ) which indicates a balanced and unbalanced
+        tree. Moreover, The BST is very cheap to serach for something
+        compared to other data structures.
+
+        The intialisation works by going over every island from the
+        list given and putting it through a formula to find its designated key.
+        Then it is inserted into a binary tree.
+
+        Example:
+        For example, if we had 3 islands ['a', 100, 300] ['b', 200, 200] ['c' 300, 200,]
+        it would be saved onto the BST in the order of 'c','b','a'.
+
+        Complexity Reasoning:
+        We needed a worst Case of O( N(LogN) ). The complexity of the code is 
+        O( N * comp ). This is because when going over N islands, we make 
+        a comparison to determine where the element should be placed which comes out
+        to O( Comp ) + O( 1 ) as shown in week 10. However getting rid of the O( 1 ),
+        we end up with O( N * comp )
         """
 
         self.Islands = BinarySearchTree()
@@ -27,7 +52,42 @@ class Mode1Navigator:
 
     def select_islands(self) -> list[tuple[Island, int]]:
         """
-        Student-TODO: Best/Worst Case
+        Worst Case: O( N )
+        Best Case: O( N )
+        
+        where N is the number of elements in the BST.
+
+        Data Structure:
+        The data structure, as implemented in initialisation, was
+        a BST with hints of a priority sort. This is because all the
+        elements in the BST are ordered from the top priority or the
+        element to be selected first, to the least.
+
+        The function works by first creating the list of tuples that 
+        will be returned. Then we go over every island in the BST to ensure that 
+        we are only sending crew members to the top most priorirty islands.
+
+        for every island welook at, the number of crew to be sent
+        is decided. unless we dont have enough crew, we send all the ones we need.
+        however if we dont have enough, then we iterate through the list and find the 
+        island that would give us the most money.
+
+        Example:
+        for example if we had 200 crew memebers and the sorted BST of:
+        
+        [ Island("A", 400, 100)
+        Island("D", 350, 90)
+        Island("E", 300, 100)
+        Island("B", 300, 150)
+        Island("C", 100, 5) ]
+
+        the function will send 100 to the first island, 90 to the second island and then serach for 
+        the next best island to send to.
+
+        Complexity reasoning:
+        The best and worst cases need to be O( N ) and O( LogN ). However the best and worst cases
+        of the function is both O( N ). This is because no matter what, the function will always 
+        look at every element to ensure that the best islands are found.
         """
 
         Current_Crew_Num = self.crew
@@ -61,7 +121,36 @@ class Mode1Navigator:
 
     def select_islands_from_crew_numbers(self, crew_numbers: list[int]) -> list[float]:
         """
-        Student-TODO: Best/Worst Case
+        Worst Case: O( C * N )
+        Best Case: O( C * N )
+        
+        Where C is the number of elements in crew_numbers
+        and N is the number of elements in the BST.
+
+        Data structure:
+        There are 2 datastructures used in the function, A BST containing all the
+        islands and a list containing all the crew numbers. 
+
+        For every crew number, we essentially rerun the select_islands(self) however 
+        instead of only running it 1 time, we run it C times where C is the 
+        number of elements in crew_numbers. We go in order of the top priority to the
+        least and send crew members accordingly.
+
+        Example:
+        if we had the crew members [ 200, 500, 5] for a sorted BST:
+        
+        [ Island("A", 400, 100)
+        Island("D", 350, 90)
+        Island("E", 300, 100)
+        Island("B", 300, 150)
+        Island("C", 100, 5) ]
+
+        we would end up with [865, 1450, 100]
+
+        Complexity Reasoning:
+        We needed to have a worst case of O( C * N ). the worst case for the function is 
+        O( C * N ) as for every crew member C, we go over every island N making it C * N.
+
         """
         counter = 0
         Crew_Numbers = crew_numbers
@@ -103,7 +192,19 @@ class Mode1Navigator:
 
     def update_island(self, island: Island, new_money: float, new_marines: int) -> None:
         """
-        Student-TODO: Best/Worst Case
+        Worst case: O(1)
+        Best Case: O(1)
+
+        data structure:
+        none
+
+        example:
+        if we update island1 with new money and marines,
+        then it will be updated.
+
+        complexity reasoning:
+        because we change an object, everything that
+        refers to that object will be effected as well.
         """
         Island = island
         Island.money = new_money
--- mode2.py
+++ mode2.py
@@ -8,7 +8,17 @@ class Mode2Navigator:
 
     def __init__(self, n_pirates: int) -> None:
         """
-        Student-TODO: Best/Worst Case
+        Worst case: O( 1 )
+        Best Case: O( 1 )
+
+        datastructure:
+        Maxheap
+
+        example:
+        if we create this class, we get a max heap.
+
+        complexity reasoning:
+        we only initialise 3 things.
         """
         self.Num_Pirates = n_pirates
         self.Islands = MaxHeap(0)
@@ -16,7 +26,34 @@ class Mode2Navigator:
 
     def add_islands(self, islands: list[Island]):
         """
-        Student-TODO: Best/Worst Case
+        Worst Case: O( N + I )
+        Best Case: O( N + I )
+
+        where N is the number of island we have already. 
+        where I is the islands being added.
+
+        data structure:
+        Max heap was used for this function. the max heap is perfect for this
+        scenerio as we need to prioritise elements and pull from the
+        data sturcture inorder.
+
+        example: 
+        if we have islands:
+        [
+        'A','B','C',
+        ]
+
+        they will be inSersted 'A','B','C' as for now, we dont care about key.
+
+        The function goes through every island given I and adds it to a new heap before 
+        going over all the islands N and adding those islands to the new heap as well before 
+        saving the new heap. This is because the heap is an array based structure and cant 
+        extend its length so we need to create a new heap every time to have enough space.
+
+        complexity reasoning:
+        every time this function is called, we insert all the new islands
+        and then all the old islands no matter what. therefore, the complexity will always be 
+        O( N + I ).
         """
 
         NewHeap = MaxHeap(self.Islands.length + len(islands))
@@ -36,7 +73,34 @@ class Mode2Navigator:
 
     def simulate_day(self, crew: int) -> list[tuple[Island|None, int]]:
         """
-        Student-TODO: Best/Worst Case
+        Best Case: O( N + C )
+        Worst Case: O( N + C )
+        
+        Where N is the amount of islands.
+        C is the number of pirates and 
+        K is the compelxity of get_max()
+
+        Data Structure:
+        This function uses a heap as intialised in the setup and alway uses a list 
+        as the data structure used to return
+
+        The function will go over every Island and reinsert them with the correct 
+        key calculated. Then for every crew member, 
+        we get the top element with get_max() and calculate the money and crew sent
+        before re inserting.
+
+        Example:
+        if we have 3 islands in the heap, then all 3 islands will be reinserted with new 
+        heaps and then calcualted over.
+
+        complexity analysis:
+        We can have a worst case of O( N + C*log(N) ). We have O( N + C ).
+        This is because we only have 2 loops and do 2 things. First we
+        reinsert every island with a new updated key making O( N ). 
+        Then for every crew member, we get the top element and get the
+        crew sent and money earned before reinserting it back into the heap.
+
+        therefore only O( N + C )
         """
         
         NewHeap = MaxHeap(self.Islands.length)
